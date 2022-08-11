966f5af3d4f6ed26e123649f1563119bec884cdc
physnerds <amitbashyal@gmail.com>
2022-08-11 10:26:28 -0500

    Some more tinkering with fundamental and non fundamental types needed for proper unpacking and forwarding of arguments from tuples

--- test_tuple.cpp
+++ test_tuple.cpp
@@ -3,7 +3,8 @@
 
 //tbb related libraries
 #include "tbb/tbb.h"
-
+#include <array>
+#include <type_traits>
 class test{
 public:
     void operator()(int a, int b, int c, int* out){
@@ -20,6 +21,7 @@ decltype(auto)apply_tuple_impl(F&& fn, Tuple&& t, std::index_sequence<S...>)
 {
     return std::forward<F>(fn)(std::get<S>(std::forward<Tuple>(t))...);
 }
+
 template<typename F, typename Tuple>
 decltype(auto) apply_from_tuple(F&& fn, Tuple && t){
     
@@ -58,30 +60,76 @@ MyTask<F, ARGS...>::MyTask(tbb::task_arena &arena, ARGS... args):m_arena(std::mo
 template<class F, typename... ARGS>
 bool MyTask<F, ARGS...>::execute(){
     auto fun = F();
-    apply_from_tuple(fun,m_tuple);
 
-  /*
-    m_arena.execute([&fun,m_tuple](){
+    std::tuple temp_tuple(std::move(m_tuple));
+    m_arena.execute([&fun,&temp_tuple](){
     tbb::task_group group;
         group.run([&](){
-            apply_from_tuple(fun,m_tuple);
+            apply_from_tuple(fun,temp_tuple);
             
         });
         group.wait();
     
     });
-*/
+
+}
+
+
+//Test whether something is tuple or not....
+template<typename T>
+struct IsTupleImpl : std::false_type{};
+  
+template<typename... U>
+struct IsTupleImpl<std::tuple<U...>>: std::true_type{};
+
+template<typename T>
+constexpr bool IsTuple(T* x){
+ return IsTupleImpl<std::decay_t<T>>::value;
+}
+
+template<typename T>
+constexpr bool IsFundamental(T x){
+    return std::is_fundamental<T>::value;
 }
 
 //now the testing of the unpacking tuples...
+template<typename T>
+struct is_pointer { static const bool value = false; };
 
-int main(){
+template<typename T>
+struct is_pointer<T*> { static const bool value = true; };
+
+//now for the arrays
+template<typename T>
+struct is_arrayImpl: std::false_type{};
+    
+template<typename T>
+struct is_arrayImpl<T[]>: std::true_type{};
+    
+template<typename T, std::size_t N>
+struct is_arrayImpl<T[N]>: std::true_type{};
+
+template<typename T>
+constexpr bool is_array(T* x){
+    std::cout<<"x is "<<x<<std::endl;
+    return is_arrayImpl<std::decay_t<T>>::value;
+}
+
+
+int main()
+{
     int a = 1;
     int b = 2;
     int c = 3;
     int* out = new int[100];
-   
-    
+    int val[100];
+    bool _true = true;
+    int arrSize = *(&out + 1) - out;
+    std::cout<<out[0]<<" Empty array "<<std::endl;
+    std::cout<<"Size of Out is "<<sizeof(out)/sizeof(out[0])<<" "<<arrSize<<std::endl;
+    std::cout<<is_array(val)<<'\n'<<is_array(&a)<<std::endl;
+    std::cout<<"fundamental "<<std::is_fundamental<int>::value<<" "<<IsFundamental(out)<<std::endl;
+    //we just need one thread to dispatch....
     tbb::task_arena arena(1);
     auto task = make_Task<test>(arena,a,b,c,out);
     
@@ -89,5 +137,13 @@ int main(){
     
     std::cout<<"Output After the Execution "<<out[0]<<std::endl;
     
+    
+    //test on make_index_sequence
+    
+    auto x_ = std::make_index_sequence<10>();
+    
+    auto test_tuple = std::make_tuple(1,2,3,4);
+    std::cout<<IsTuple(&test_tuple)<<std::endl;
     return 1;
 }
+
